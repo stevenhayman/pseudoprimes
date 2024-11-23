cb7d1fad3b33fde4cff6211b012e6fa60ea800cd
Steven Hayman <steven.paul.hayman@gmail.com>
2024-11-22 18:55:59 -0600

    Cleanup main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-# import json
-# from math import log
-# import time
-# from pseudoprimes.erdos_construction import construct_carmichael_number
-# from pseudoprimes.erdos_construction_varient import _generate_divisors_less_than, build_primes_set, build_primes_set_partition
-# from pseudoprimes.factorization import DivisorsIterator, Factorization
-# from pseudoprimes.fellows_koblitz import is_prime, order
-# from pseudoprimes.highly_composite_numbers import generate_candidate_highly_composite_numbers_less_than
-# from pseudoprimes.integers import int_log, int_root
-# from pseudoprimes.naive_subset_sum import find_first_non_empty_subset, find_non_empty_subsets
-# from pseudoprimes.partitioned_divisors_iterator import PartitionedDivisorsIterator
-# from pseudoprimes.trial_division import factor
+"""Main module for the application."""
 
 def main() -> None:
-    pass
+    """Main entry point for the application."""
 
 if __name__ == '__main__':
     main()
